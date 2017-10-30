95ee0ab52ec9fd7953a801c8d136fc359ee9b1d4
Max <maximilian.muza@gmx.de>
2017-10-30 14:52:48 +0100

    interactive navigationItems

--- resources/views/includes/navigationTop.blade.php
+++ resources/views/includes/navigationTop.blade.php
@@ -5,16 +5,16 @@
 				<img class="navigation__logo" src="/img/Mm.png">
 			</a>
 		</div>
-		<div class="navigationItem">
+		<div class="navigationItem sliding-middle-out">
 			<a href="">PROZESS</a>
 		</div>
-		<div class="navigationItem">
+		<div class="navigationItem sliding-middle-out">
 			<a href="">KONTAKT</a>
 		</div>
-		<div class="navigationItem">
+		<div class="navigationItem sliding-middle-out">
 			<a href="">LEISTUNGEN</a>
 		</div>
-		<div class="navigationItem">
+		<div class="navigationItem sliding-middle-out">
 			<a href="">FAQ</a>
 		</div>
 	</div>
