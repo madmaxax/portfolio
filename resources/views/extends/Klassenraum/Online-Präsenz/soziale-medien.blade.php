262553cfb17f5919570b68006164515af99799e7
Max <maximilian.muza@gmx.de>
2017-12-06 09:34:17 +0100

    mediaIcons text update

--- resources/views/extends/Klassenraum/Online-Präsenz/soziale-medien.blade.php
+++ resources/views/extends/Klassenraum/Online-Präsenz/soziale-medien.blade.php
@@ -102,20 +102,6 @@
 		</div>
 	</div>
 </div>
-<div class="mediaIcons__outerCard">
-	<div class="mediaIcons__innerCard flex">
-		<h3>Teile deine Meinung mit!</h3>
-		<div class="mediaIcons__container flex">
-			<a target="_blank" href="https://plus.google.com/114290443080274459125">
-				<i class="icon-gplus"></i>
-			</a>
-			<a target="_blank" href="https://www.facebook.com/maximilianmuza/">
-				<i class="icon-facebook-squared"></i>
-			</a>
-			<a target="_blank" href="https://www.linkedin.com/in/maximilianmuza/">
-				<i class="icon-linkedin-squared"></i>
-			</a>
-		</div>
-	</div>
-</div>
+@include('/includes/_mediaIcons')
+
 @stop
---
+++ resources/views/includes/_mediaIcons.blade.php
@@ -0,0 +1,16 @@
+<div class="mediaIcons__outerCard">
+	<div class="mediaIcons__innerCard flex">
+		<h3>Deine Meinung ist mir wichtig!</h3>
+		<div class="mediaIcons__container flex">
+			<a target="_blank" href="https://plus.google.com/114290443080274459125">
+				<i class="icon-gplus"></i>
+			</a>
+			<a target="_blank" href="https://www.facebook.com/maximilianmuza/">
+				<i class="icon-facebook-squared"></i>
+			</a>
+			<a target="_blank" href="https://www.linkedin.com/in/maximilianmuza/">
+				<i class="icon-linkedin-squared"></i>
+			</a>
+		</div>
+	</div>
+</div>
